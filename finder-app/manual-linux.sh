3f73f49fb48c24580df67a95f40eb821ec8a0211
Denys Raduchych <raduchych.denys@gmail.com>
2024-12-08 12:14:21 +0100

    Fixed branches mergin mistakes

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -58,12 +58,6 @@ mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 
-mkdir "${OUTDIR}/rootfs"
-cd "${OUTDIR}/rootfs"
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log
-
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
